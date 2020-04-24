693b4b39f4a7e44387b84057a484fbb1bdba0db5
Corinne Bintz <cbintz@middlebury.edu>
2020-04-24 10:48:26 -0700

    kmeans progress and some rap songs

--- feature_extract.py
+++ feature_extract.py
@@ -12,6 +12,15 @@ x1 , sr = librosa.load(pop1, sr = 44100, offset = 10, duration = 30)
 x2 , sr = librosa.load(pop2, sr = 44100, offset = 10, duration = 30)
 x3 , sr = librosa.load(pop3, sr = 44100, offset = 10, duration = 30)
 
+
+rap1 = '/Users/corinnebintz/Desktop/701/701beats/rap/SUGAR.m4a'
+rap2 = '/Users/corinnebintz/Desktop/701/701beats/rap/3005.m4a'
+rap3 = '/Users/corinnebintz/Desktop/701/701beats/rap/HUMBLE.m4a'
+
+x4 , sr = librosa.load(rap1, sr = 44100, offset = 10, duration = 30)
+x5 , sr = librosa.load(rap2, sr = 44100, offset = 10, duration = 30)
+x6 , sr = librosa.load(rap3, sr = 44100, offset = 10, duration = 30)
+
 #feature extraction
 
 #BPMs
@@ -24,7 +33,16 @@ tempo2 = librosa.beat.tempo(onset_envelope=onset_env2, sr=sr)
 onset_env3 = librosa.onset.onset_strength(x3, sr=sr)
 tempo3 = librosa.beat.tempo(onset_envelope=onset_env3, sr=sr)
 
-print(tempo1, tempo2, tempo3)
+onset_env4 = librosa.onset.onset_strength(x4, sr=sr)
+tempo4 = librosa.beat.tempo(onset_envelope=onset_env4, sr=sr)
+
+onset_env5 = librosa.onset.onset_strength(x5, sr=sr)
+tempo5 = librosa.beat.tempo(onset_envelope=onset_env5, sr=sr)
+
+onset_env6 = librosa.onset.onset_strength(x5, sr=sr)
+tempo6 = librosa.beat.tempo(onset_envelope=onset_env6, sr=sr)
+
+print(tempo1, tempo2, tempo3, tempo4, tempo5, tempo6)
 
 
 #chromagrams
@@ -39,12 +57,25 @@ chroma2 = scale(chroma_cq2, axis=1)
 chroma_cens3 = librosa.feature.chroma_cens(x3, sr=sr)
 chroma_cq3 = librosa.feature.chroma_cqt(x3, sr=sr)
 chroma3 = scale(chroma_cq3, axis=1)
-print("chroma")
-print(chroma3)
+
+chroma_cens4 = librosa.feature.chroma_cens(x4, sr=sr)
+chroma_cq4 = librosa.feature.chroma_cqt(x1, sr=sr)
+chroma4 = scale(chroma_cq4, axis=1)
+
+chroma_cens5 = librosa.feature.chroma_cens(x5, sr=sr)
+chroma_cq5 = librosa.feature.chroma_cqt(x5, sr=sr)
+chroma5 = scale(chroma_cq5, axis=1)
+
+chroma_cens6 = librosa.feature.chroma_cens(x6, sr=sr)
+chroma_cq6 = librosa.feature.chroma_cqt(x6, sr=sr)
+chroma6 = scale(chroma_cq6, axis=1)
 
 chroma1_linear = [i for sublist in chroma1 for i in sublist]
 chroma2_linear = [i for sublist in chroma2 for i in sublist]
 chroma3_linear = [i for sublist in chroma3 for i in sublist]
+chroma4_linear = [i for sublist in chroma4 for i in sublist]
+chroma5_linear = [i for sublist in chroma5 for i in sublist]
+chroma6_linear = [i for sublist in chroma6 for i in sublist]
 
 plt.figure()
 plt.subplot(2,1,1)
@@ -74,6 +105,23 @@ mfcc3_delta = librosa.feature.delta(mfcc3, mode='nearest')
 mfcc3_complete = np.concatenate((mfcc3, mfcc3_delta), axis=0)
 mfcc3_scale = scale(mfcc3_complete, axis=1)
 
+mfcc4 = librosa.feature.mfcc(x4, sr=sr)
+mfcc4_delta = librosa.feature.delta(mfcc4, mode='nearest')
+mfcc4_complete = np.concatenate((mfcc4, mfcc4_delta), axis=0)
+mfcc4_scale = scale(mfcc4_complete, axis=1)
+
+mfcc5 = librosa.feature.mfcc(x5, sr=sr)
+mfcc5_delta = librosa.feature.delta(mfcc5, mode='nearest')
+mfcc5_complete = np.concatenate((mfcc5, mfcc5_delta), axis=0)
+mfcc5_scale = scale(mfcc5_complete, axis=1)
+
+mfcc6 = librosa.feature.mfcc(x6, sr=sr)
+mfcc6_delta = librosa.feature.delta(mfcc6, mode='nearest')
+mfcc6_complete = np.concatenate((mfcc6, mfcc6_delta), axis=0)
+mfcc6_scale = scale(mfcc6_complete, axis=1)
+
+
+
 #https://github.com/smarsland/AviaNZ/blob/master/Clustering.py
 mfcc1_linear = [i for sublist in mfcc1_scale for i in sublist]
 
@@ -82,6 +130,12 @@ mfcc2_linear = [i for sublist in mfcc2_scale for i in sublist]
 
 
 mfcc3_linear = [i for sublist in mfcc3_scale for i in sublist]
+
+mfcc4_linear = [i for sublist in mfcc4_scale for i in sublist]
+
+mfcc5_linear = [i for sublist in mfcc5_scale for i in sublist]
+
+mfcc6_linear = [i for sublist in mfcc6_scale for i in sublist]
 
 
 plt.figure(figsize=(10, 4))
@@ -93,10 +147,10 @@ plt.show()
 
 #KMEANS STEP
 
-chroma = [np.array(chroma1_linear), np.array(chroma2_linear), np.array(chroma3_linear)]
-mfcc = [np.array(mfcc1_linear), np.array(mfcc2_linear), np.array(mfcc3_linear)]
+chroma = [np.array(chroma1_linear), np.array(chroma2_linear), np.array(chroma3_linear), np.array(chroma4_linear), np.array(chroma5_linear), np.array(chroma6_linear)]
+mfcc = [np.array(mfcc1_linear), np.array(mfcc2_linear), np.array(mfcc3_linear), np.array(mfcc4_linear), np.array(mfcc5_linear), np.array(mfcc6_linear)]
 
-tempo = np.array([tempo1, tempo2, tempo3])
+tempo = np.array([tempo1, tempo2, tempo3, tempo4, tempo5, tempo6])
 
 kmeans = KMeans(n_clusters=2, max_iter=100).fit(mfcc)
 print([kmeans.labels_, kmeans.cluster_centers_])
